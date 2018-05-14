243569bfa5d843b2ce413740345e81e558644a99
Abel ko <esung1129@gmail.com>
2018-05-14 19:12:18 +0900

    fakeaddress 만들기, 및 실험용 prompt

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,8 +8,8 @@ class Form extends Component {
   }
 
   handleSubmit(event) {
-    console.log(`${process.env.REACT_APP_PLATING_API_URL}/canteen/v3/company`)
-    
+    console.log(`${process.env.REACT_APP_PLATING_API_URL}/canteen/v3/company`);
+
     event.preventDefault();
     const data = new FormData(event.target);
 
@@ -24,10 +24,9 @@ class Form extends Component {
       detail: inputsArr[3]
     };
 
-
     // api local연결시.
     // fetch("http://localhost:25056/canteen/v3/company", {
-      fetch(`${process.env.REACT_APP_PLATING_API_URL}/canteen/v3/company`, {
+    fetch(`${process.env.REACT_APP_PLATING_API_URL}/canteen/v3/company`, {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -43,52 +42,63 @@ class Form extends Component {
           console.log("fetch fail... OTL");
         }
       })
-      .then((data)=>{
-          if(data) {
-            const { newUrl } = data;
-            console.log(newUrl);
-            alert(`             아래 주소를 통해 새로 생긴 페이지를 확인하세요
+      .then(data => {
+        if (data) {
+          const { newUrl } = data;
+          console.log(newUrl);
+          alert(`             아래 주소를 통해 새로 생긴 페이지를 확인하세요
             https://canteen.plating.co.kr/${newUrl}`);
-          }
+        }
       })
       .catch(err => {
         console.log("acquire err !!!!" + err.message);
       });
   }
 
+  prompt() {
+    const address = prompt("hi")
+    console.log(address);
+  }
+
   render() {
     return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <div className="myInput">
-          <span>Name</span>
-          <input id="name" name="name" type="text" placeholder="플레이팅" />
-        </div>
-        <div className="myInput">
-          <span>URL</span>
-          <input id="url" name="url" type="text" placeholder="plating" />
-        </div>
-        <div className="myInput">
-          <span>Address</span>
-          <input
-            id="address"
-            name="address"
-            type="text"
-            placeholder="서울 강남구 논현동 122-8"
-          />
-        </div>
-        <div className="myInput">
-          <span>Detail</span>
-          <input
-            id="detail"
-            name="detail"
-            type="text"
-            placeholder="동화히스토리 B1"
-          />
-        </div>
-        <div className="button">
-          <button>추가</button>
-        </div>
-      </form>
+      <div>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <div className="myInput">
+            <span>Name</span>
+            <input id="name" name="name" type="text" placeholder="플레이팅" />
+          </div>
+          <div className="myInput">
+            <span>URL</span>
+            <input id="url" name="url" type="text" placeholder="plating" />
+          </div>
+          <div className="myInput">
+            <span>Address</span>
+            <div className="fakeInput-wrapper" onClick={this.prompt}>
+              <div className="fakeInput">서울 강남구 논현동 122-8</div>
+            </div>
+              <input
+                id="address"
+                name="address"
+                type="hidden"
+                value="모달에서 받아온값"
+                placeholder="서울 강남구 논현동 122-8"
+              />
+          </div>
+          <div className="myInput">
+            <span>Detail</span>
+            <input
+              id="detail"
+              name="detail"
+              type="text"
+              placeholder="동화히스토리 B1"
+            />
+          </div>
+          <div className="button">
+            <button>추가</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
