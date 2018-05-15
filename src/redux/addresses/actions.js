33e5a5b35b11e2dbe2211a1a518b784598236756
Abel ko <esung1129@gmail.com>
2018-05-15 15:41:48 +0900

    address actions 작성

--- src/redux/addresses/actions.js
+++ src/redux/addresses/actions.js
@@ -1,13 +1,33 @@
-export const NAME = 'ADDRESS';
+export const NAME = "ADDRESS";
 
 export const LOAD_ADDRESSES = `${NAME}/LOAD_ADDRESSES`;
 export const LOAD_ADDRESSES_REQUEST = `${NAME}/LOAD_ADDRESSES_REQUEST`;
 export const LOAD_ADDRESSES_SUCCESS = `${NAME}/LOAD_ADDRESSES_SUCCESS`;
 export const LOAD_ADDRESSES_FAILURE = `${NAME}/LOAD_ADDRESSES_FAILURE`;
 
-export function loadAddress(query) {
+export function loadAddresses(query) {
+  return {
+    type: LOAD_ADDRESSES,
+    query,
+  };
+}
+
+export function loadAddressesRequest(query) {
+  return {
+    type: LOAD_ADDRESSES_REQUEST,
+    query,
+  };
+}
+
+export function loadAddressesSuccess(addresses) {
     return {
-        type: LOAD_ADDRESSES,
-        query,
+        type: LOAD_ADDRESSES_SUCCESS,
+        addresses,
     }
+}
+
+export function loadAddressesFailure() {
+    return {
+        type: LOAD_ADDRESSES_FAILURE,
+    };
 }
--- src/redux/addresses/sagas.js
+++ src/redux/addresses/sagas.js
@@ -1,29 +1,29 @@
-import { takeEvery, put, call } from 'redux-saga/effects';
+import { takeEvery, put, call } from "redux-saga/effects";
 
-import { getAddressInfo } from 'src/api';
+import { getAddressInfo } from "src/api";
 
 import {
-    LOAD_ADDRESSES,
-    loadAddressesRequest,
-    loadAddressesSuccess,
-    loadAddressesFailure,
-} from './actions';
+  LOAD_ADDRESSES,
+  loadAddressesRequest,
+  loadAddressesSuccess,
+  loadAddressesFailure
+} from "./actions";
 
 // loadAddresses 제너레이터 함수는 action객체를 받고,
 export function* loadAddresses(actions) {
-    const { query } = actions;
-    yield put(loadAddressesRequest(query));
-    try {
-        const addresses = yield call(getAddressInfo, {
-            query,
-        });
-    
-      yield put(loadAddressesSuccess(addresses));
-    } catch(err) {
-      yield put(loadAddressesFailure(err));
-    }
+  const { query } = actions;
+  yield put(loadAddressesRequest(query));
+  try {
+    const addresses = yield call(getAddressInfo, {
+      query
+    });
+
+    yield put(loadAddressesSuccess(addresses));
+  } catch (err) {
+    yield put(loadAddressesFailure(err));
+  }
 }
 
 export function* watchAddresses() {
-    yield takeEvery(LOAD_ADDRESSES, loadAddresses);
+  yield takeEvery(LOAD_ADDRESSES, loadAddresses);
 }
